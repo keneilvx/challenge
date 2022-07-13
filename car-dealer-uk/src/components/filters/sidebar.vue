f49480708b287f9396e15561d6fc58738d3d46db
Keneil Smith <66431476+keneilvx@users.noreply.github.com>
2022-07-13 22:23:48 +0100

    Created sidebar component

---
+++ car-dealer-uk/src/components/filters/sidebar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="row">
+        <div>
+          <p>Filters</p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
