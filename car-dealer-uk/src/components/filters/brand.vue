41c41e01e526936d1d1bf7d63422e48f28afe260
Keneil Smith <66431476+keneilvx@users.noreply.github.com>
2022-07-16 17:01:34 +0100

    filter for brands

---
+++ car-dealer-uk/src/components/filters/brand.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <p class="heading__subtitle">Brand</p>
+    <div>
+      <div><span>Make</span></div>
+      <div><span>Model</span></div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "brand"
+}
+</script>
+
+<style scoped>
+
+</style>
