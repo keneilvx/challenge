2bc70c28d2146ef026933389350e65b8af39ef88
Keneil Smith <66431476+keneilvx@users.noreply.github.com>
2022-07-16 16:58:02 +0100

    basic scaffold for finance banner

--- car-dealer-uk/src/components/finance-banner/banner.vue
+++ car-dealer-uk/src/components/finance-banner/banner.vue
@@ -1,12 +1,36 @@
 <template>
-<div class="row">
-  <div class="finance-banner">
-    <div class="bg-circle">
-      <p>Finance a new or used car</p>
-      <p>Adjust the options to find the finance
-        arrangement that suits you.
-      </p>
+
+  <div class="finance-banner d-none  d-xl-block ">
+    <div class="row d-flex justify-content-around align-items-center">
+    <div class="oval col-lg-3" style="">
+      <div class="p-2" >
+        <div class="col-9 p-2">
+          <p class="finance-banner__title">Finance a new or used car</p>
+          <p class="finance-banner__subtitle">Adjust the options to find the finance
+            arrangement that suits you.
+          </p>
+        </div>
+      </div>
+
     </div>
+    <div class="col-lg-9">
+      <div class="row">
+        <div class="col" v-for="(select, index) in types">
+
+          <div>
+        <span>
+          <span>{{select.name}}</span>
+        </span>
+            <select style="background: #3F3A50;
+border-radius: 16px; width: 217px;
+height: 50px;">
+              <option>Select</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+
 
   </div>
 </div>
@@ -14,7 +38,32 @@
 
 <script>
 export default {
-  name: "index.vue"
+  name: "index.vue",
+  data(){
+    return{
+      types:[
+        {
+          name: 'Deposit',
+          options: []
+        },
+        {
+          name: 'Mileage',
+          options: []
+        },
+        {
+          name: 'Period',
+          options: []
+        },
+        {
+          name: 'Budget',
+          options: []
+        }
+      ]
+
+
+    }
+  }
+
 }
 </script>
 
