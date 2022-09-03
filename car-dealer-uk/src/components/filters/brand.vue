a21991d00c1fa9d29115dfbf2a83cb248c47ecf1
Keneil Smith <66431476+keneilvx@users.noreply.github.com>
2022-09-03 22:05:57 +0100

    Filter dropdown component

--- car-dealer-uk/src/components/filters/brand.vue
+++ car-dealer-uk/src/components/filters/brand.vue
@@ -1,19 +1,69 @@
 <template>
-<div>
-  <p class="heading__subtitle">Brand</p>
+  <div>
+    <p class="heading__subtitle">Brand</p>
     <div>
-      <div><span>Make</span></div>
+      <div class="row">
+        <div class="col-9">
+          <div><span>Make</span></div>
+        </div>
+        <div class="col-2">
+          <a
+            class=""
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#panelsStayOpen-collapseOne"
+            aria-expanded="true"
+            aria-controls="panelsStayOpen-collapseOne"
+          >
+            <remove-collapse></remove-collapse>  
+          </a>
+        </div>
+        <div class="accordion" id="accordionPanelsStayOpenExample">
+          <div class="accordion-item">
+            <div
+              id="panelsStayOpen-collapseOne"
+              class="accordion-collapse collapse show"
+              aria-labelledby="panelsStayOpen-headingOne"
+            >
+              <div class="accordion-body">
+                <div class="d-flex-wrap">
+                  <div class="">
+                    <options :item="test"/>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div><span>Model</span></div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
+  import Options from './components/options.vue';
+  import removeCollapse from '../icons/remove-collapse.vue';
 export default {
-  name: "brand"
-}
+  name: "brand",
+data(){
+  return{
+      test:"Land Rover"
+  }
+},
+  components:
+  {
+    Options,
+    removeCollapse
+  }
+};
 </script>
 
 <style scoped>
 
+.accordion-item{
+  border: none !important;
+}
+  
 </style>
---
+++ car-dealer-uk/src/components/filters/components/options.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="options-tag">
+        <div class="row">
+            <div class="col-8 d-flex">
+                {{item}}
+            </div>
+            <div class="col-4 d-flex justify-content-end">
+                <remove-collapse></remove-collapse>
+            </div>
+            
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import removeCollapse from '../../icons/remove-collapse.vue';
+  export default {
+    name: "brand",
+    props: {
+        item: ''
+    },
+    components:{
+        removeCollapse
+    }
+  
+ 
+  };
+  </script>
+  
+
+<style>
+    
+    .options-tag{
+        border-radius: 8px;
+        background-color: aliceblue;
+        padding: 5px;
+       
+    }
+</style>
