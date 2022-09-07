f1e75a1cb317cb5f9d336da7b41db51345f0ff26
Keneil Smith <66431476+keneilvx@users.noreply.github.com>
2022-09-07 22:44:14 +0100

    created the filter dropdown component

---
+++ car-dealer-uk/src/components/filters/components/filter-dropdown.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="filters-button">
+        <div class="row">
+        <div class="col-9">
+          <div><span>{{filter_name}}</span></div>
+        </div>
+        <div class="col-2">
+          <a
+            class=""
+            type="button"
+            data-bs-toggle="collapse"
+            :data-bs-target="'#'+ filter_name"
+            aria-expanded="true"
+            aria-controls="panelsStayOpen-collapseOne"
+          >
+           <add-expanded></add-expanded>
+          </a>
+        </div>
+        <div class="accordion" id="accordionPanelsStayOpenExample">
+          <div class="accordion-item">
+            <div
+              :id="filter_name"
+              class="accordion-collapse collapse close"
+              aria-labelledby="panelsStayOpen-headingOne"
+            >
+              <div class="accordion-body">
+                <div class="d-flex-wrap">
+                  <div class="row">
+                    <div>
+                      
+                    </div>
+                    <options :item="filter_options"/>
+                    
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+</template>
+
+
+<script>
+    import addExpanded from '../../icons/add-expanded.vue';
+import options from './options.vue';
+    export default{
+        components: {
+            options,
+            addExpanded
+        },
+
+        props:{
+            filter_name: '',
+            filter_id: '',
+            filter_list: null,
+            filter_options: ''
+
+        }
+    }
+
+</script>
+
+<style>
+    .accordion-item{
+        border: none !important;
+    }
+
+    .filters-button{
+        padding: 10px;
+        border-radius: 10px;
+    }
+
+
+</style>
---
+++ car-dealer-uk/src/components/icons/add-expanded.vue
@@ -0,0 +1,7 @@
+<template>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M8 12L16 12" stroke="#D1D6E0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M12 16L12 8" stroke="#D1D6E0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <rect x="0.5" y="0.5" width="23" height="23" rx="7.5" stroke="#D1D6E0"/>
+    </svg>
+ </template>
