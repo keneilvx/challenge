c8361ef81359bac321e09a4cbeaae516ea6e2e5a
Keneil Smith <66431476+keneilvx@users.noreply.github.com>
2022-09-08 22:56:28 +0100

    Update budget.vue

--- car-dealer-uk/src/components/filters/budget.vue
+++ car-dealer-uk/src/components/filters/budget.vue
@@ -2,20 +2,26 @@
   <div>
     <p class="heading__subtitle">Budget</p>
     <div>
-      <div><span>Min. Price</span></div>
-      <div><span>Max. Price</span></div>
+      <filter-dropdown
+      :filter_id="'min'" :filter_name="'Min. Price'" :filter-options="'Min. Price'"
+      ></filter-dropdown>
+      <filter-dropdown
+      :filter_id="'max'" :filter_name="'Max. Price'" :filter-options="'Max. Price'"
+      ></filter-dropdown>
     </div>
   </div>
 </template>
 
 <script>
   import FilterDropdown from './components/filter-dropdown.vue';
+  import AddExpanded from '../icons/add-expanded.vue';
 export default {
 
   name: "budget",
 
   components: {
-    FilterDropdown
+    FilterDropdown,
+    AddExpanded,
   }
 }
 </script>
