66cb8c749f98c662772ce6a27e5cd9a20b369547
Keneil Smith <66431476+keneilvx@users.noreply.github.com>
2022-09-11 22:50:04 +0100

    Added Lowest price dropdown

--- car-dealer-uk/src/components/card/listing-type.vue
+++ car-dealer-uk/src/components/card/listing-type.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
     <div class="row mb-4">
-      <div class="pills-section">
-        <span class="fw-bold mr-3 cr-pl-2"> Showing {{ total }} cars</span>
-        
-        <button class=" ml-5 pill-showing active m-1" style="background-color: #7572ff; border: none;">All</button>
-        <button class="pill-showing m-1">Used</button>
-        <button class="pill-showing m-1">New</button>
-        <button class="pill-showing m-1">Offers</button>
+      <div class="col-8">
+        <div class="pills-section">
+          <span class="fw-bold mr-3 cr-pl-2"> Showing {{ total }} cars</span>
+          <button class=" ml-5 pill-showing active m-1" style="background-color: #7572ff; border: none;">All</button>
+          <button class="pill-showing m-1">Used</button>
+          <button class="pill-showing m-1">New</button>
+          <button class="pill-showing m-1">Offers</button>
+        </div>
       </div>
 
+      <div class="col-4">
+        <div class="d-flex justify-content-end pr-cl-2">
+          <select class="dropdown__main">
+            <option>
+              lowest price
+            </option>
+          </select>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -35,6 +45,10 @@ export default {
 <style scoped>
   .cr-pl-2{
     padding: 20px;
+  }
+
+  .pr-cl-2{
+    padding-right: 20px;
   }
 
 </style>
